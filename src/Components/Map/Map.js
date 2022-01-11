0f3063acf62437fe9261edfdd0b7e1fab812664a
Brice Bridges <bcb35b@gmail.com>
2022-01-11 09:02:52 -0700

    Added base marker feature to cental Den

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,11 +2,35 @@ import React, { useRef, useState, useEffect } from "react"
 import "./Map.css";
 import MapContext from "./MapContext";
 import * as ol from "ol";
+import {Point} from 'ol/geom'
+import {fromLonLat} from 'ol/proj'
+import {Vector} from 'ol/source'
+import * as layer from 'ol/layer'
 
 const Map = ({ children, zoom, center }) => {
   
   const mapRef = useRef();
   const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    if (!map) return;
+    
+    var marker = new ol.Feature({
+      geometry: new Point(
+        fromLonLat([-104.991531, 39.742043])
+      ),
+    });
+
+    var vectorSource = new Vector({
+      features: [marker]
+    })
+
+    var markerVectorLayer = new layer.Vector({
+      source: vectorSource,
+    });
+
+    map.addLayer(markerVectorLayer);
+  })
 
   // on component mount
   useEffect(() => {
